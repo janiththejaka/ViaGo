743cd661cf90def7e489dbce065a7cc127fcc50d
JanithThejaka <janiththejaka2002@gmail.com>
2025-11-16 20:47:48 +0530

    initial configurations

--- ViaGo/src/App.jsx
+++ ViaGo/src/App.jsx
@@ -1,12 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './Login';
 import './index.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Welcome to ViaGo </h1>
-      <h4>your ride... your choice...</h4>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">Welcome to ViaGo</h1>
+      <h4 className="text-xl text-gray-200 mt-2">your ride... your choice...</h4>
     </div>
   );
 }
