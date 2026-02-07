7eac9cab962f0350739131f44842b7883ff8fbfc
JanithThejaka <janiththejaka2002@gmail.com>
2026-02-07 14:37:26 +0530

    configurations for the further developments

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import UserLoging from './pages/Users/UserLoging'
 import UserSignup from './pages/Users/UserSignup'
 import DriverLanding from './pages/Drivers/DriverLanding'
 import DriverSignup from './pages/Drivers/DriverSignup'
+import WebSocket from './pages/Websocket'
+import LiveTrackingMap from './components/LiveTrackingMap'
+//import RideRequest from './components/RideRequest'
+import RideRequestPage from './pages/RideRequestPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
@@ -14,6 +19,26 @@ function App() {
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/drive" element={<DriverLanding />} />
         <Route path="/driver-signup" element={<DriverSignup />} />
+        <Route path="/ride-request-page" element={<RideRequestPage />} />
+
+
+        <Route
+          path="/websocket"
+          element={
+            <ProtectedRoute>
+              <WebSocket />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/live-tracking"
+          element={
+            <ProtectedRoute>
+              <LiveTrackingMap />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </Router>
   )
