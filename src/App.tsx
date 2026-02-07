6ca9fa73f9e73e58f28e1c6ce1508280bcfb7bc5
naveenilaksha <naveennilaksha15@gmail.com>
2026-02-08 01:31:29 +0530

    routing for the new driver side pages and route protecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,89 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import UserHome from './pages/Users/UserHome'
 import UserLoging from './pages/Users/UserLoging'
 import UserSignup from './pages/Users/UserSignup'
 import DriverLanding from './pages/Drivers/DriverLanding'
 import DriverSignup from './pages/Drivers/DriverSignup'
+import DriverDashboard from './pages/Drivers/DriverDashboard'
+import DriverHistory from './pages/Drivers/DriverHistory'
+import DriverEarnings from './pages/Drivers/DriverEarnings'
 import WebSocket from './pages/Websocket'
 import LiveTrackingMap from './components/LiveTrackingMap'
-//import RideRequest from './components/RideRequest'
-import RideRequestPage from './pages/RideRequestPage'
+import RideRequestPage from './pages/RideRequestPage.tsx'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UserHome />} />
-        <Route path="/login" element={<UserLoging />} />
-        <Route path="/signup" element={<UserSignup />} />
-        <Route path="/drive" element={<DriverLanding />} />
-        <Route path="/driver-signup" element={<DriverSignup />} />
-        <Route path="/ride-request-page" element={<RideRequestPage />} />
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<UserHome />} />
+          <Route path="/login" element={<UserLoging />} />
+          <Route path="/signup" element={<UserSignup />} />
+          <Route path="/drive" element={<DriverLanding />} />
+          <Route path="/driver-signup" element={<DriverSignup />} />
+          <Route path="/driver-dashboard" element={<DriverDashboard />} />
+          <Route path="/driver-history" element={<DriverHistory />} />
+          <Route path="/driver-earnings" element={<DriverEarnings />} />
+          <Route path="/ride-request-page" element={<RideRequestPage />} />
 
 
-        <Route
-          path="/websocket"
-          element={
-            <ProtectedRoute>
-              <WebSocket />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/live-tracking"
-          element={
-            <ProtectedRoute>
-              <LiveTrackingMap />
-            </ProtectedRoute>
-          }
-        />
+          {/* RIDER Protected Routes */}
+          <Route
+            path="/ride-request-page"
+            element={
+              <ProtectedRoute allowedRole="RIDER">
+                <RideRequestPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/websocket"
+            element={
+              <ProtectedRoute allowedRole="RIDER">
+                <WebSocket />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/live-tracking"
+            element={
+              <ProtectedRoute allowedRole="RIDER">
+                <LiveTrackingMap />
+              </ProtectedRoute>
+            }
+          />
 
-      </Routes>
-    </Router>
+          {/* DRIVER Protected Routes */}
+          <Route
+            path="/driver-dashboard"
+            element={
+              <ProtectedRoute allowedRole="DRIVER">
+                <DriverDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/driver-history"
+            element={
+              <ProtectedRoute allowedRole="DRIVER">
+                <DriverHistory />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/driver-earnings"
+            element={
+              <ProtectedRoute allowedRole="DRIVER">
+                <DriverEarnings />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
